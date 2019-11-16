78246a20f39bbb0e004723cb5f48431923cf9508
wildhevire <wildhevire404@gmail.com>
2019-11-16 20:58:27 +0700

    initial commit, including basic game mechanic

---
+++ Assets/Scripts/PickupsSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupsSpawner : MonoBehaviour
+{
+   
+    //public List<Vector2> pickupPos;
+    Vector2[] pickupPos;
+    public int banyakPickup = 20;
+    [SerializeField] GameObject pickup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupPos = new Vector2[banyakPickup];
+        for (int i = 0; i < banyakPickup; i++)
+        {
+            pickupPos[i] = randomPos();
+            Instantiate(pickup, new Vector3(pickupPos[i].x, pickupPos[i].y, 0), Quaternion.identity);
+        }
+
+        //foreach (Vector2 pos in pickupPos) {
+        //    Vector2 tmp = randomPos();
+        //    pickupPos.Add(tmp);
+        //    Instantiate(pickup, new Vector3(pickupPos[i].x, pickupPos[i].y, 0), Quaternion.identity);
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    Vector2 randomPos()
+    {
+
+        var x = Random.Range(-38, 38);
+        var y = Random.Range(-12, 12);
+
+        return new Vector2(x, y);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 origin, target;
+    float distance;
+    public float maxForce = 5;
+    public float Force = 5;
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigid = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            origin = this.transform.position;
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position;
+            float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            this.transform.rotation = rotation;
+
+            distance = Vector2.Distance(origin, target);
+            distance = Mathf.Clamp(distance, 0, maxForce);
+            Debug.DrawRay(origin, -target.normalized * distance);
+        }else
+        if (Input.GetMouseButtonUp(0)) {
+            rigid.velocity = -target.normalized * distance;
+        }
+    }
+
+    //private void OnMouseDrag()
+    //{
+    //    origin = this.transform.position;
+    //    target = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position;
+    //    float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+    //    Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    //    this.transform.rotation = rotation;
+        
+    //    distance = Vector2.Distance(origin, target);
+    //    distance = Mathf.Clamp(distance, 0, maxForce);
+    //    Debug.DrawRay(origin, -target.normalized * distance);
+    //}
+    //private void OnMouseUp()
+    //{
+    //    //rigid.AddForce(-target.normalized * distance * Force);
+    //    rigid.velocity = -target.normalized * distance;
+    //}
+}
